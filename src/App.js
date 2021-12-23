dccdce207a47563fc4a2a1a90748327507881a8f
sanin-mohd <sanin.nitcalicut.ece@gmail.com>
2021-12-23 08:35:58 +0530

    adding new list completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react/cjs/react.development";
+import "./App.css";
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState("");
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const d = new Date();
+  let day = d.getDay();
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br/>
+        <h2>Whoop, it's {days[day]} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(event) => {
+            setToDo(event.target.value);
+          }}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() => {
+            setToDos([...toDos, {id:Date.now(), text:toDo,status:false}]);
+          }}
+          className="fas fa-plus"
+        ></i>
+      </div>
+      <div className="todos">
+        {toDos.map((obj) => {
+          return (
+            <div className="todo">
+              <div className="left">
+                <input onChange={(event)=>{
+                  console.log(event.target.value);
+                  console.log(obj);
+                  setToDos(toDos.filter(obj2=>{
+                  if(obj2.id===obj.id){
+                    obj2.status= !obj.status
+                  }
+                  return obj2
+                }))}}  value={obj.status} type="checkbox" name="" id="" />
+                <p >{obj.text}</p>
+              </div>
+              <div className="right">
+                <i onClick={()=>{setToDo('')}} className="fas fa-times"></i>
+              </div>
+            </div>
+          );
+        })}
+
+        {
+          toDos.map((obj)=>{
+            if(obj.status===true){
+              return(
+                <h1>{obj.text}</h1>
+              )
+            }
+            return null;
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Counter() {
+    
+    return (
+        <div>
+            <h1>I am a component</h1>
+        </div>
+    )
+}
+
+export default Counter
